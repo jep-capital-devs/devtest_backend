fc79c05281d5232d55a035a0571e788b5fae626f
Brian Ellis <brian@briansresume.com>
2019-11-04 17:43:58 -0500

    tested modules.sh script

--- app/Frostbite/Test/FrostUsers/Data/_____FrostUsersDataCreator.php
+++ app/Frostbite/Test/FrostUsers/Data/_____FrostUsersDataCreator.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UsersDataCreator extends Migration
+class FrostUsersDataCreator extends Migration
 {
     /**
      * Run the migrations.
--- bin/modules.sh
+++ bin/modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ..
-php artisan migrate --path="application/app/Frostbite/Test/FrostUsers/Data/_____FrostUsersDataCreator.php"
+php artisan migrate --path="app/Frostbite/Test/FrostUsers/Data/_____FrostUsersDataCreator.php"
 php artisan cache:clear
