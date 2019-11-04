8d276a340126d7473ebee6743da120bfbab11d1e
Brian Ellis <brian@briansresume.com>
2019-11-04 17:18:50 -0500

    file fixes

--- bin/setup_script.sh
+++ bin/setup_script.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-sudo composer install
-sudo php artisan key:generate
-sudo php artisan migrate
-sudo php artisan db:seed
-sudo php artisan voyager:install
+cd ..
+composer install
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
+php artisan voyager:install
 
 clear
 echo "===================="
 echo "=== CREATE ADMIN ==="
 echo "===================="
-sudo php artisan voyager:admin --create
+php artisan voyager:admin --create
 
 clear
 echo "==================="
 echo "= FRONT-END TOOLS ="
 echo "==================="
 cd resources/views/themes/frostbite/assets
-sudo npm install && sudo gulp css && sudo gulp lintjs
+npm install && gulp css && gulp lintjs
+
+clear
+echo "============================"
+echo "= SETTING FILE PERMISSIONS ="
+echo "============================"
+cd ../../../../..
+find ./ -type f -exec chmod 664 {} \;
+find ./ -type d -exec chmod 775 {} \;
+
+clear
+echo "INSTALL FINISHED. YOU CAN BEGIN WORKING NOW!"
