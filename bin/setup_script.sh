58bdf16e48764d2acaf92013555c7e62f0a38126
Brian Ellis <brian@briansresume.com>
2019-11-07 13:07:28 -0500

    remove file perm

--- bin/setup_script.sh
+++ bin/setup_script.sh
@@ -23,15 +23,6 @@ if test -f "$ISROOTDIR"; then
   cd resources/views/themes/frostbite/assets
   npm install && gulp css && gulp lintjs
 
-  clear
-  echo "============================"
-  echo "= SETTING FILE PERMISSIONS ="
-  echo "============================"
-  cd ../../../../..
-  find ./ -type f -exec chmod 664 {} \;
-  find ./ -type d -exec chmod 775 {} \;
-
-  clear
   echo "INSTALL FINISHED. YOU CAN BEGIN WORKING NOW!"
 else
   echo "You need to be in a Frostbite application root directory before continuing"
